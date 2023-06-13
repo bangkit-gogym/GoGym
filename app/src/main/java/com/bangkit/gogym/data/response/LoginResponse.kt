5b390e0649a4946fa6c89353baeb42386177dd2d
Dikamahard <ikkyboss@gmail.com>
2023-06-13 17:18:20 +0700

    testing login api

--- app/src/main/java/com/bangkit/gogym/data/response/LoginResponse.kt
+++ app/src/main/java/com/bangkit/gogym/data/response/LoginResponse.kt
@@ -14,12 +14,13 @@ data class LoginResponse(
 )
 
 data class LoginResult(
+
+    @field:SerializedName("token")
+    val token: String? = null,
+
     @field:SerializedName("name")
     val name: String? = null,
 
-    @field:SerializedName("userId")
-    val userId: String? = null,
-
-    @field:SerializedName("token")
-    val token: String? = null
+    @field:SerializedName("id")
+    val id: String? = null
 )
